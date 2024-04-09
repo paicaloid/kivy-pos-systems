eb1066305f3269fa69104a2c3217b8ac83408d38
paicaloid <paicaloid@gmail.com>
2024-04-10 01:05:02 +0700

    Finish login module with mock auth

--- views/auth/auth.py
+++ views/auth/auth.py
@@ -5,6 +5,21 @@ from kivy.uix.boxlayout import BoxLayout
 Builder.load_file("views/auth/auth.kv")
 
 
+def mock_authenticate(username: str, password: str) -> bool:
+    if username == "admin" and password == "admin":
+        return True
+    return False
+
+
 class Auth(BoxLayout):
     def __init__(self, **kw) -> None:
         super().__init__(**kw)
+
+    def authenticate(self):
+        if mock_authenticate(
+            username=self.ids.username.text, password=self.ids.password.text
+        ):
+            print("Authenticated!")
+            App.get_running_app().root.ids.scrn_mngr.current = "scrn_home"
+        else:
+            print("Authentication failed!")
