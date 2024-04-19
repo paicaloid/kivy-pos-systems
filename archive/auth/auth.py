16d993cdb522938a4caae56f59768ddc2a60bfd8
paicaloid <paicaloid@gmail.com>
2024-04-19 11:38:01 +0700

    Add archive

---
+++ archive/auth/auth.py
@@ -0,0 +1,25 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+
+Builder.load_file("views/auth/auth.kv")
+
+
+def mock_authenticate(username: str, password: str) -> bool:
+    if username == "admin" and password == "admin":
+        return True
+    return False
+
+
+class Auth(BoxLayout):
+    def __init__(self, **kw) -> None:
+        super().__init__(**kw)
+
+    def authenticate(self):
+        if mock_authenticate(
+            username=self.ids.username.text, password=self.ids.password.text
+        ):
+            print("Authenticated!")
+            App.get_running_app().root.ids.scrn_mngr.current = "scrn_menu"
+        else:
+            print("Authentication failed!")
