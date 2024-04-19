29acf846abc9fbac30f9834500a8b3771fcd935f
paicaloid <paicaloid@gmail.com>
2024-04-19 11:21:40 +0700

    Add new auth page

--- views/auth/auth.py
+++ views/auth/auth.py
@@ -1,25 +1,33 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import BooleanProperty
 from kivy.uix.boxlayout import BoxLayout
 
 Builder.load_file("views/auth/auth.kv")
 
 
-def mock_authenticate(username: str, password: str) -> bool:
-    if username == "admin" and password == "admin":
-        return True
-    return False
+class WelcomeModule(BoxLayout):
+    def __init__(self, **kw) -> None:
+        super().__init__(**kw)
+
+
+class LoginModule(BoxLayout):
+    login_btn_disabled = BooleanProperty(True)
+
+    def __init__(self, **kw) -> None:
+        super().__init__(**kw)
+
+    def authenticate(self):
+        print("Authenticated!")
+        App.get_running_app().root.ids.scrn_mngr.current = "scrn_menu"
+
+    def check_input(self):
+        if self.ids.username.text == "" or self.ids.password.text == "":
+            self.login_btn_disabled = True
+        else:
+            self.login_btn_disabled = False
 
 
 class Auth(BoxLayout):
     def __init__(self, **kw) -> None:
         super().__init__(**kw)
-
-    def authenticate(self):
-        if mock_authenticate(
-            username=self.ids.username.text, password=self.ids.password.text
-        ):
-            print("Authenticated!")
-            App.get_running_app().root.ids.scrn_mngr.current = "scrn_home"
-        else:
-            print("Authentication failed!")
